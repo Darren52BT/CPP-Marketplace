86fd03bff28815a6bb0868c9b32ebe3c5a88cf1c
Cesar de Paula <cdepaula942@gmail.com>
2024-02-02 18:49:47 -0800

    fixing some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import LoginPage from "./components/LoginPage";
+import NavBar from "./components/NavBar";
+import ProductCarousel from "./components/ProductCarousel/ProductCarousel";
 
+let prods = [
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0070/7032/files/trending-products_c8d0d15c-9afc-47e3-9ba2-f7bad0505b9b.png?format=jpg&quality=90&v=1614559651",
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0070/7032/files/trending-products_c8d0d15c-9afc-47e3-9ba2-f7bad0505b9b.png?format=jpg&quality=90&v=1614559651",
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0070/7032/files/trending-products_c8d0d15c-9afc-47e3-9ba2-f7bad0505b9b.png?format=jpg&quality=90&v=1614559651",
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0070/7032/files/trending-products_c8d0d15c-9afc-47e3-9ba2-f7bad0505b9b.png?format=jpg&quality=90&v=1614559651",
+  },
+];
 function App() {
-  return <div><LoginPage /></div>
+  return (
+    <><NavBar/>
+      <ProductCarousel products={prods} />
+    </>
+  );
 }
 
 export default App;
