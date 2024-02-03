6602ee307176170b0db148bed037efd8231cbd3a
Nicholas Amancio <nick.amancio44@gmail.com>
2024-02-02 17:41:54 -0800

    made the navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import "./Navbar.css";
+
+function NavBar() {
+    return(
+        <Navbar expand="lg" className="navbar-custom">
+        <Container>
+            <Nav.Link href="#"><img className="hamburger-column" src="src\photos\Hamburger_icon.png" alt="hamburger" /></Nav.Link>
+            <Navbar.Brand href="#home" className="fs-4">CPP MARKETPLACE</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="ms-auto">
+                <Nav.Link href="#"><img id="noti" src="src\photos\bell.png" alt="notification" className="img-fluid" /></Nav.Link>
+                <Nav.Link href="#"><img id="pfp" src="src\photos\user.png" alt="pfp" className="img-fluid" /></Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+    );
+}
+
+export default NavBar;
