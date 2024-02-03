d362ea7ed0532dc5943d326ac69d26138b4d6e73
Cesar de Paula <cdepaula942@gmail.com>
2024-02-03 02:48:12 -0800

    added item carousel for item page

--- frontend/src/components/ItemPage.jsx
+++ frontend/src/components/ItemPage.jsx
@@ -1,13 +1,32 @@
-import './ItemPage.css';
+// import './ItemPage.css';
+// function ItemPage() {
+//     return (
+//         <div className="container">
+//             <img className = "itemImg" src= "https://upload.wikimedia.org/wikipedia/commons/2/22/Twice_-_Dickies_Arena%2C_2022_%28cropped%29.jpg"></img>
+//             <div className= "text">
+//                 <h1>This is an Item</h1>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default ItemPage;
+
+import ItemCarousel from "./ItemCarousel";
+
 function ItemPage() {
+    let images = [
+        {
+          image:
+            "https://i.ebayimg.com/images/g/Nb8AAOSwR3tlsXLe/s-l1600.jpg"
+        },
+    ];
+
     return (
-        <div className="container">
-            <img className = "itemImg" src= "https://upload.wikimedia.org/wikipedia/commons/2/22/Twice_-_Dickies_Arena%2C_2022_%28cropped%29.jpg"></img>
-            <div className= "text">
-                <h1>This is an Item</h1>
-            </div>
+        <div className="ItemPage">
+            <ItemCarousel images={images}/>
         </div>
-    );
+    )
 }
 
 export default ItemPage;
